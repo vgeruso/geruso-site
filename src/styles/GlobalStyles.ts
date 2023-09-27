44b8dd9439f54a50e2ac4e48334c58c646dd3419
vgeruso <victor.geruso@gmail.com>
2023-09-27 01:33:21 -0300

    [ADD] GlobalStyles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    margin: 0;
+    padding: 0;
+    background-color: #F3F3F3;
+  }
+`;
