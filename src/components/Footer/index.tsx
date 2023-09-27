935975ac8c7d7a1b29b3cda3dec32e0568f9421c
vgeruso <victor.geruso@gmail.com>
2023-09-27 01:32:36 -0300

    [ADD] Footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+const Footer: FC = () => {
+  return (
+    <footer>
+      <div>
+        <h1>FOOTER</h1>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
