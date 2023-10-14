c8320567e9fe4e6c1db9a1700aaabf514a68d28e
vgeruso <victor.geruso@gmail.com>
2023-10-14 02:09:59 -0300

    [ADD] Component Card Info

---
+++ src/components/CardInfo/index.tsx
@@ -0,0 +1,54 @@
+import React, {FC} from "react";
+
+import { 
+  ContainerDark,
+  ImgProfile,
+  HeadContainer, 
+  ImgContainer, 
+  TitleContainer, 
+  Title, 
+  Subtitle
+} from "./styles";
+import image from '../../images/image.jpg';
+import SkillBar from "../skillBar";
+
+// TODO Aplicar os estilos [Stiled-component]
+const CardInfo: FC = () => {
+  return (
+    <div>
+      <ContainerDark>
+        <HeadContainer>
+          <ImgContainer>
+            <ImgProfile src={image} alt="profile"/>
+          </ImgContainer>
+          <TitleContainer>
+            <Title>Victor Geruso Mota Pereira Gomes</Title>
+            <Subtitle>Engenheiro de Software</Subtitle>
+          </TitleContainer>
+        </HeadContainer>
+        <div style={{padding: '20px'}}>
+          <h3 style={{color: '#cecece'}}>Habilidades</h3>
+          <div style={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center'}}>
+            <SkillBar level="adivanced" tool="Git"/>
+            <SkillBar level="adivanced" tool="JavaScript"/>
+            <SkillBar level="adivanced" tool="NodeJS"/>
+            <SkillBar level="adivanced" tool="ReactJS"/>
+            <SkillBar level="adivanced" tool="PostgreSQL"/>
+            <SkillBar level="adivanced" tool="MySQL"/>
+            <SkillBar level="adivanced" tool="Scrum"/>
+            <SkillBar level="adivanced" tool="Docker"/>
+            <SkillBar level="middle" tool="TypeScript"/>
+            <SkillBar level="middle" tool="React Native"/>
+            <SkillBar level="middle" tool="NextJS"/>
+            <SkillBar level="middle" tool="NestJS"/>
+            <SkillBar level="middle" tool="Prisma"/>
+            <SkillBar level="middle" tool="Jest"/>
+            <SkillBar level="basic" tool="S.O.L.I.D"/>
+          </div>
+        </div>
+      </ContainerDark>
+    </div>
+  )
+}
+
+export default CardInfo;
---
+++ src/components/CardInfo/styles.ts
@@ -0,0 +1,50 @@
+import { styled } from 'styled-components';
+
+export const ContainerDark = styled.div`
+  background-color: #2a2c37;
+  margin: 20px;
+  padding: 25px;
+  border-radius: 10px;
+`;
+
+export const HeadContainer = styled.div`
+  border-bottom: 1px solid #cecece;
+  display: flex;
+  flex-direction: row;
+  height: 30vh;
+  overflow: hidden;
+`;
+
+export const ImgContainer = styled.div`
+  width: 25%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ImgProfile = styled.img`
+  height: 90%;
+  border-radius: 10em;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 75%;
+`;
+
+export const Title = styled.h1`
+  color: #cecece;
+  margin-bottom: 0;
+  font-size: 3.5em;
+  font-weight: bold;
+`;
+
+export const Subtitle = styled.h3`
+  color: #6c6c6c;
+  margin-top: 0;
+  font-size: 1.5em;
+`;
