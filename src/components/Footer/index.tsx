c363af4c4563bb13d696e7bee5ec998d4f976de6
vgeruso <victor.geruso@gmail.com>
2023-10-14 01:56:56 -0300

    [ADJ] Images on Head footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,9 @@ import React, { FC, useState } from "react";
 
 import { ContainerDark } from "./styles";
 
-import { IconGitHubDark, IconLinkedIn, IconXDark } from "../Icons";
+import gitHub from '../../images/gh.svg';
+import linkedIn from '../../images/linkedin.svg';
+import x from '../../images/x.svg';
 
 const Footer: FC = () => {
   const [year, _] = useState(new Date().getFullYear());
@@ -12,13 +14,13 @@ const Footer: FC = () => {
         <div className="centralize">
           <div className="icons">
             <a href="https://github.com/vgeruso" target="__blank">
-              <IconGitHubDark/>
+              <img src={gitHub} alt="gitHub"/>
             </a>
             <a href="https://twitter.com/VGeruso" target="__blank">
-              <IconXDark/>
+              <img src={x} alt="x"/>
             </a>
             <a href="https://www.linkedin.com/in/victor-geruso-gomes-654a8111a/" target="__blank">
-              <IconLinkedIn/>
+              <img src={linkedIn} alt="linkedin" />
             </a>
           </div>
           <p className="justify">
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,50 +1,23 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 
 import {
-  ContainerDark, 
-  ContainerLight,
+  ContainerDark,
   LinkDark, 
-  LinkLight
 } from "./styles";
 
-import { LogoDark, LogoLight } from "../Logo";
-import { 
-  IconGitHubDark,
-  IconGitHubLight,
-  IconThemeDark,
-  IconThemeLight
-} from "../Icons";
+import logo from "../../images/Logo.svg";
+import gitHub from "../../images/gh.svg";
 
 const Header: FC = () => {
-  const [theme, setTheme] = useState('dark');
-  
-  const handleTheme = (theme: string) => {
-    setTheme(theme);
-  }
-
   return (
     <header>
-      {theme === 'light' ? (
-        <ContainerLight>
-          <LogoLight/>
-          <LinkLight onClick={() => handleTheme('dark')}>
-            <IconThemeLight/>
-          </LinkLight>
-          <LinkLight href="https://github.com/vgeruso" target="__blank">
-            <IconGitHubLight/>
-          </LinkLight>
-        </ContainerLight>
-      ) : (
         <ContainerDark>
-          <LogoDark/>
-          <LinkDark onClick={() => handleTheme('light')}>
-            <IconThemeDark/>
-          </LinkDark>
+          {/* <LogoDark/> */}
+          <img src={logo} alt="logo"/>
           <LinkDark href="https://github.com/vgeruso" target="__blank">
-            <IconGitHubDark/>
+            <img src={gitHub} alt="gitHub"/>
           </LinkDark>
         </ContainerDark>
-      )}
     </header>
   );
 }
